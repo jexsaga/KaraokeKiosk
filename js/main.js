3e3805f3931bbf915a646a2698774ae00059f80f
Jessica Robbins <jessicarobbins03@gmail.com>
2026-04-05 21:05:49 +0200

    sertfgyhui

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 // js for the index.html
 
+function switchTabs(switchTo){
+    const musicTab = document.getElementById("music-tab");
+    const menuTab = document.getElementById("food-tab");
+    if (switchTo == "music"){
+        musicTab.style.display = "flex";
+        menuTab.style.display = "none";
+    } else {
+        musicTab.style.display = "none";
+        menuTab.style.display = "flex";
+    }
+}
+
 const musicBtn = document.getElementById("musicBtn");
 const menuBtn = document.getElementById("menuBtn");
+musicBtn.addEventListener('click', () => switchTabs("music"));
+menuBtn.addEventListener('click', () => switchTabs("menu"));
+
+
 
